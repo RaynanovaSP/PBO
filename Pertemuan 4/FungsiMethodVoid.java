f3faf065403fdb670fcc341c9365ab4aa2565a9d
RaynanovaSP <71371196+RaynanovaSP@users.noreply.github.com>
2020-10-27 11:47:41 +0700

    Add files via upload

---
+++ Pertemuan 4/FungsiMethodVoid.java
@@ -0,0 +1,26 @@
+
+
+public class FungsiMethodVoid {
+
+    public static void main (String[] args){
+
+        // void itu artinya hampa
+        System.out.println(simpel());
+        fungsiVoid("apa pun");
+        selamatPagi("emak");
+        selamatPagi("abah");
+    }
+    private static void selamatPagi(String nama){
+        System.out.println("selamat Pagi " + nama);
+    }
+    // fungsi atau method tanpa kembalian
+    private static void fungsiVoid(String input){
+        System.out.println(input);
+    }
+    // fungsi atau method dengan kembalian
+    // sehingga menggunakan return untuk
+    // mengembalikan nilainya (10.0f)
+    private static float simpel(){
+        return 10.0f;
+    }
+}
---
+++ Pertemuan 4/JavaScope.java
@@ -0,0 +1,10 @@
+
+
+public class JavaScope {
+    public static void main(String[] args){
+        //Code here Can't use X
+        int x = 100;
+        //Code here can use x
+        System.out.println(x);
+    }
+}
